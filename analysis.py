ad6e41c2687d5a9569e91ff6ce94b5cb8743ea28
Hugo Kitano <hkitano@stanford.edu>
2019-12-01 16:46:12 -0800

    simple scripts

--- analysis.py
+++ analysis.py
@@ -12,6 +12,7 @@ import utils.processing as proc
 
 
 def main():
+    pdb.set_trace()
     zenodo_path = './data/'
     deepmoon_path = os.path.dirname(os.getcwd())
     train_imgs = h5py.File(zenodo_path + 'train_images.hdf5', 'r')
@@ -23,7 +24,7 @@ def main():
 
     ctrs = pd.HDFStore(zenodo_path + 'train_craters.hdf5', 'r')
 
-    pdb.set_trace()
+    # pdb.set_trace()
 
     Image.MAX_IMAGE_PIXELS = None
     img = Image.open(zenodo_path + "/LunarLROLrocKaguya_118mperpix.png").convert("L")
@@ -38,22 +39,50 @@ def main():
     sample_data = {'imgs': [gen_imgs['input_images'][...].astype('float32'),
                             gen_imgs['target_masks'][...].astype('float32')]}
     proc.preprocess(sample_data) #now, input images is shape 25 x 256 x 256 x 1, target_masks is shape 25 x 256 x 256
+
     sd_input_images = sample_data['imgs'][0] #25 x 256 x 256 x 1 
-    sd_target_masks = sample_data['imgs'][1] #25 x 256 x 256
+    sd_target_masks = sample_data['imgs'][1] #25 x 256 x 256p
+
 
     # Plot the data for fun.
     fig = plt.figure(figsize=[12, 6])
     [ax1, ax2] = fig.subplots(1,2)
     ax1.imshow(sd_input_images[5].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
     ax2.imshow(sd_target_masks[5].squeeze(), origin='upper', cmap='Greys_r')
-    plt.show()
-    plt.savefig("plots/processed_image_and_mask.png")
+    plt.savefig("plots/processed_image_and_mask1.png")
+
+    fig = plt.figure(figsize=[12, 6])
+    [ax1, ax2] = fig.subplots(1,2)
+    ax1.imshow(sd_input_images[8].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+    ax2.imshow(sd_target_masks[8].squeeze(), origin='upper', cmap='Greys_r')
+    plt.savefig("plots/processed_image_and_mask2.png")
+
+    fig = plt.figure(figsize=[12, 6])
+    [ax1, ax2] = fig.subplots(1,2)
+    ax1.imshow(sd_input_images[12].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+    ax2.imshow(sd_target_masks[12].squeeze(), origin='upper', cmap='Greys_r')
+    plt.savefig("plots/processed_image_and_mask3.png")
+
+    fig = plt.figure(figsize=[12, 6])
+    [ax1, ax2] = fig.subplots(1,2)
+    ax1.imshow(sd_input_images[16].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+    ax2.imshow(sd_target_masks[16].squeeze(), origin='upper', cmap='Greys_r')
+    plt.savefig("plots/processed_image_and_mask4.png")
 
     sample_images = train_imgs['input_images'][0:20]
     sample_masks = train_imgs['target_masks'][0:20]
 
-    model = load_model('models/model_keras1.2.2.h5')
+    pdb.set_trace()
+    model = load_model('models/model_30k.h5')
 
+        #### TEST CRATERS 
+    test_imgs = h5py.File(zenodo_path + '/test_images.hdf5', 'r')
+
+    test_data = {'imgs': [test_imgs['input_images'][...].astype('float32'),
+                        test_imgs['target_masks'][...].astype('float32')]}
+    proc.preprocess(test_data)
+    sd_input_images = test_data['imgs'][0]
+    sd_target_masks = test_data['imgs'][1]
 
 
     iwant = 3
@@ -73,8 +102,48 @@ def main():
     ax2.set_title('Ground-Truth Target Mask')
     ax3.set_title('CNN Predictions')
     ax4.set_title('Post-CNN Craters')
-    plt.show()
-    plt.savefig("plots/trained_model_results.png")
+    plt.savefig("plots/trained_model_results1.png")
+
+    iwant = 6
+    pred = model.predict(sd_input_images[iwant:iwant + 1])
+    extracted_rings = tmt.template_match_t(pred[0].copy(), minrad=2.) # x coord, y coord, radius
+
+    fig = plt.figure(figsize=[16, 16])
+    [[ax1, ax2], [ax3, ax4]] = fig.subplots(2, 2)
+    ax1.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+    ax2.imshow(sd_target_masks[iwant].squeeze(), origin='upper', cmap='Greys_r')
+    ax3.imshow(pred[0], origin='upper', cmap='Greys_r', vmin=0, vmax=1)
+    ax4.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap="Greys_r")
+    for x, y, r in extracted_rings:
+        circle = plt.Circle((x, y), r, color='blue', fill=False, linewidth=2, alpha=0.5)
+        ax4.add_artist(circle)
+    ax1.set_title('Moon DEM Image')
+    ax2.set_title('Ground-Truth Target Mask')
+    ax3.set_title('CNN Predictions')
+    ax4.set_title('Post-CNN Craters')
+    plt.savefig("plots/trained_model_results2.png")
+
+    iwant = 13
+    pred = model.predict(sd_input_images[iwant:iwant + 1])
+    extracted_rings = tmt.template_match_t(pred[0].copy(), minrad=2.) # x coord, y coord, radius
+
+    fig = plt.figure(figsize=[16, 16])
+    [[ax1, ax2], [ax3, ax4]] = fig.subplots(2, 2)
+    ax1.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+    ax2.imshow(sd_target_masks[iwant].squeeze(), origin='upper', cmap='Greys_r')
+    ax3.imshow(pred[0], origin='upper', cmap='Greys_r', vmin=0, vmax=1)
+    ax4.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap="Greys_r")
+    for x, y, r in extracted_rings:
+        circle = plt.Circle((x, y), r, color='blue', fill=False, linewidth=2, alpha=0.5)
+        ax4.add_artist(circle)
+    ax1.set_title('Moon DEM Image')
+    ax2.set_title('Ground-Truth Target Mask')
+    ax3.set_title('CNN Predictions')
+    ax4.set_title('Post-CNN Craters')
+    plt.savefig("plots/trained_model_results3.png")
+    
+
+    
     
 
 if __name__ == '__main__':
---
+++ sample_plots.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import h5py
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+import pdb
+from keras.models import load_model
+from utils import template_match_target as tmt
+import input_data_gen as igen
+import utils.processing as proc
+
+
+def main():
+    pdb.set_trace()
+    zenodo_path = './data/'
+    model = load_model('models/model_30k.h5')
+
+       #### TEST CRATERS 
+    test_imgs = h5py.File(zenodo_path + '/test_images.hdf5', 'r')
+
+    test_data = {'imgs': [test_imgs['input_images'][...].astype('float32'),
+                        test_imgs['target_masks'][...].astype('float32')]}
+    proc.preprocess(test_data)
+    sd_input_images = test_data['imgs'][0]
+    sd_target_masks = test_data['imgs'][1]
+
+    images = [1,10,16]
+    plot_dir = "sample_plots"
+
+    for iwant in images:
+        pred = model.predict(sd_input_images[iwant:iwant + 1])
+        extracted_rings = tmt.template_match_t(pred[0].copy(), minrad=2.) # x coord, y coord, radius
+
+        fig = plt.figure(figsize=[16, 16])
+        [[ax1, ax2], [ax3, ax4]] = fig.subplots(2, 2)
+        ax1.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap='Greys_r', vmin=0, vmax=1.1)
+        ax2.imshow(sd_target_masks[iwant].squeeze(), origin='upper', cmap='Greys_r')
+        ax3.imshow(pred[0], origin='upper', cmap='Greys_r', vmin=0, vmax=1)
+        ax4.imshow(sd_input_images[iwant].squeeze(), origin='upper', cmap="Greys_r")
+        for x, y, r in extracted_rings:
+            circle = plt.Circle((x, y), r, color='blue', fill=False, linewidth=2, alpha=0.5)
+            ax4.add_artist(circle)
+        ax1.set_title('Moon DEM Image')
+        ax2.set_title('Ground-Truth Target Mask')
+        ax3.set_title('CNN Predictions')
+        ax4.set_title('Post-CNN Craters')
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        plot_location = os.path.join(current_dir, plot_dir + "/trained_model_results" + str(iwant) + ".png")
+        plt.savefig(plot_location)
+
+    
+
+    
+    
+
+if __name__ == '__main__':
+    main()
